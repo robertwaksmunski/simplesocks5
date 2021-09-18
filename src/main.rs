7d70740a24783bace736ef333aca926299c10a2d
Robert <robert@panamco.com>
2021-09-18 07:14:21 +0200

    Cleanup names & remove extra thread

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,12 @@ fn main() {
     let listener = TcpListener::bind(&config.bind_addr[..])
         .expect(&format!("Can't bind to {:#?}", &config.bind_addr));
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
+    for client_stream in listener.incoming() {
+        match client_stream {
+            Ok(client_stream) => {
+                let _ = client_stream.set_nodelay(true);
                 let _ = std::thread::spawn(move || {
-                    connection_handshake(&stream)
+                    connection_handshake(&client_stream)
                         .unwrap_or_else(|e| eprintln!("Connection error: {}", e));
                 });
             }
@@ -33,38 +34,38 @@ fn main() {
     }
 }
 
-fn connection_handshake(stream: &TcpStream) -> Result<()> {
-    let byte = stream
+fn connection_handshake(client_stream: &TcpStream) -> Result<()> {
+    let byte = client_stream
         .bytes()
         .next()
         .ok_or(error("Connection handshake read failed"))??;
     match byte {
-        5 => authentication_negotiation(stream),
+        5 => authentication_negotiation(client_stream),
         _ => Err(error("Invalid protocol version")),
     }
 }
 
-fn authentication_negotiation(mut stream: &TcpStream) -> Result<()> {
-    let authentication_methods_count = stream
+fn authentication_negotiation(mut client_stream: &TcpStream) -> Result<()> {
+    let authentication_methods_count = client_stream
         .bytes()
         .next()
         .ok_or(error("Authentication method count read failed"))??;
     let mut authentication_methods = vec![0; authentication_methods_count.into()];
-    stream.read_exact(&mut authentication_methods)?;
+    client_stream.read_exact(&mut authentication_methods)?;
 
     // Allow unauthenticated user
     if authentication_methods.contains(&0) {
-        stream.write(&[5u8, 0u8])?;
-        return process_request(stream);
+        client_stream.write(&[5u8, 0u8])?;
+        return process_request(client_stream);
     }
 
-    stream.write(&[5u8, 255u8])?;
+    client_stream.write(&[5u8, 255u8])?;
     Err(error("No acceptable authentication method sent"))
 }
 
-fn process_request(mut stream: &TcpStream) -> Result<()> {
+fn process_request(mut client_stream: &TcpStream) -> Result<()> {
     let mut request = vec![0u8; 4];
-    stream.read_exact(&mut request)?;
+    client_stream.read_exact(&mut request)?;
 
     // Protocol: always 5
     if request[0] != 5 {
@@ -72,12 +73,12 @@ fn process_request(mut stream: &TcpStream) -> Result<()> {
     }
     // Command: 1 connect, 3 udp
     if request[1] != 1 && request[1] != 3 {
-        stream.write(&[5u8, 7u8])?;
+        client_stream.write(&[5u8, 7u8])?;
         return Err(error("Request, Invalid command"));
     }
     // Reserved: always 0
     if request[2] != 0 {
-        stream.write(&[5u8, 1u8])?;
+        client_stream.write(&[5u8, 1u8])?;
         return Err(error("Request, Invalid reserved"));
     }
     // Address type: 1 IPv4, 4 IPv6
@@ -91,14 +92,14 @@ fn process_request(mut stream: &TcpStream) -> Result<()> {
     } else if request[3] == 1 {
         address_type = AddressType::IPv4;
     } else {
-        stream.write(&[5u8, 8u8])?;
+        client_stream.write(&[5u8, 8u8])?;
         return Err(error("Request, address type not supported"));
     }
 
     let request_ip = match address_type {
         AddressType::IPv4 => {
             let mut request_addr = vec![0u8; 6];
-            stream.read_exact(&mut request_addr)?;
+            client_stream.read_exact(&mut request_addr)?;
             SocketAddr::new(
                 IpAddr::V4(Ipv4Addr::new(
                     request_addr[0],
@@ -111,7 +112,7 @@ fn process_request(mut stream: &TcpStream) -> Result<()> {
         }
         AddressType::IPv6 => {
             let mut request_addr = vec![0u8; 18];
-            stream.read_exact(&mut request_addr)?;
+            client_stream.read_exact(&mut request_addr)?;
             SocketAddr::new(
                 IpAddr::V6(Ipv6Addr::new(
                     u16::from_be_bytes([request_addr[0], request_addr[1]]),
@@ -130,97 +131,95 @@ fn process_request(mut stream: &TcpStream) -> Result<()> {
     if VERBOSE > 0 {
         println!(
             "Client {} connected to {} requests {}",
-            &stream.peer_addr()?,
-            &stream.local_addr()?,
+            &client_stream.peer_addr()?,
+            &client_stream.local_addr()?,
             request_ip
         );
     }
 
     let remote = TcpStream::connect(request_ip);
     match remote {
-        Ok(remote_stream) => {
-            let mut local_reader = stream.try_clone()?;
-            let mut local_writer = stream.try_clone()?;
-            let mut remote_reader = remote_stream.try_clone()?;
-            let mut remote_writer = remote_stream.try_clone()?;
+        Ok(mut remote_stream) => {
+            let _ = remote_stream.set_nodelay(true);
 
-            local_writer.write(&[5u8, 0u8, 0u8])?;
+            client_stream.write(&[5u8, 0u8, 0u8])?;
             let local_addr = remote_stream.local_addr()?;
             match local_addr.ip() {
                 IpAddr::V4(ip) => {
-                    local_writer.write(&[1u8])?;
-                    local_writer.write(&ip.octets())?;
+                    client_stream.write(&[1u8])?;
+                    client_stream.write(&ip.octets())?;
                 }
                 IpAddr::V6(ip) => {
-                    local_writer.write(&[4u8])?;
-                    local_writer.write(&ip.octets())?;
+                    client_stream.write(&[4u8])?;
+                    client_stream.write(&ip.octets())?;
                 }
             }
-            local_writer.write(&local_addr.port().to_le_bytes())?;
+            client_stream.write(&local_addr.port().to_le_bytes())?;
 
-            let sender = std::thread::spawn(move || -> Result<()> {
-                let mut buffer = [0u8; 16384];
-                loop {
-                    match local_reader.read(&mut buffer) {
-                        Ok(read) => {
-                            if read > 0 {
-                                if VERBOSE > 2 {
-                                    println!("< {}", &read);
-                                }
-                                remote_writer.write(&buffer[0..read])?;
-                            } else {
-                                if VERBOSE > 1 {
-                                    println!("Sender EOF");
-                                }
-                                let _ = remote_writer.shutdown(Shutdown::Both);
-                                break;
-                            }
-                        }
-                        Err(e) => {
-                            eprintln!("Sender error: {}", e);
-                            let _ = remote_writer.shutdown(Shutdown::Both);
-                            break;
-                        }
-                    }
-                }
-                Ok(())
-            });
+            let mut client_stream_clone = client_stream.try_clone()?;
+            let mut remote_stream_clone = remote_stream.try_clone()?;
+
             let receiver = std::thread::spawn(move || -> Result<()> {
                 let mut buffer = [0u8; 16384];
                 loop {
-                    match remote_reader.read(&mut buffer) {
+                    match remote_stream_clone.read(&mut buffer) {
                         Ok(read) => {
                             if read > 0 {
                                 if VERBOSE > 2 {
                                     println!("> {}", &read);
                                 }
-                                local_writer.write(&buffer[0..read])?;
+                                client_stream_clone.write(&buffer[0..read])?;
                             } else {
                                 if VERBOSE > 1 {
                                     println!("Receiver EOF");
                                 }
-                                let _ = local_writer.shutdown(Shutdown::Both);
+                                let _ = client_stream_clone.shutdown(Shutdown::Both);
                                 break;
                             }
                         }
                         Err(e) => {
                             eprintln!("Receiver error: {}", e);
-                            let _ = local_writer.shutdown(Shutdown::Both);
+                            let _ = client_stream_clone.shutdown(Shutdown::Both);
                             break;
                         }
                     }
                 }
                 Ok(())
             });
-            sender
-                .join()
-                .expect("The request sender thread has panicked")?;
+
+            {
+                let mut buffer = [0u8; 16384];
+                loop {
+                    match client_stream.read(&mut buffer) {
+                        Ok(read) => {
+                            if read > 0 {
+                                if VERBOSE > 2 {
+                                    println!("< {}", &read);
+                                }
+                                remote_stream.write(&buffer[0..read])?;
+                            } else {
+                                if VERBOSE > 1 {
+                                    println!("Sender EOF");
+                                }
+                                let _ = remote_stream.shutdown(Shutdown::Both);
+                                break;
+                            }
+                        }
+                        Err(e) => {
+                            eprintln!("Sender error: {}", e);
+                            let _ = remote_stream.shutdown(Shutdown::Both);
+                            break;
+                        }
+                    }
+                }
+            }
+
             receiver
                 .join()
                 .expect("The request receiver thread has panicked")?;
         }
         Err(_) => {
-            stream.write(&[5u8, 5u8])?;
+            client_stream.write(&[5u8, 5u8])?;
             return Err(error("Request, connection failed"));
         }
     }
